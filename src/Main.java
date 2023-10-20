4b9ce5952ad5695cc33b77b80f5a8563a9f6908a
YengSokroza <yengsokroza@gmail.com>
2023-10-20 10:44:37 +0700

    lambda forEach

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.forEach( n -> System.out.println(n));
+
+    }
+
+
+}
